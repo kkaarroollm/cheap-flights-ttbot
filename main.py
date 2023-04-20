7238f2ecae702c2d20d3c083eccb96d1e4a95348
karol marszalek <mkarol.4514@gmail.com>
2023-04-20 03:33:09 +0200

    scrapes all info of all cheapest 51 destinations

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+
 from selenium import webdriver
-from selenium.common import NoSuchElementException
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,7 +9,10 @@ from time import sleep
 
 PATH = '/Users/karolmarszalek/chromedriver.exe'
 driver = webdriver.Chrome(PATH)
-
+# change mouse pointer cause kayak is different lol
+action = ActionChains(driver)
+action.move_by_offset(5, 5)
+driver.maximize_window()
 driver.get('https://www.kayak.com/explore/WAW-anywhere?tripdurationrange=1,10')
 
 
@@ -21,54 +26,57 @@ while True:
     try:
         more_destinations = wait_for_destinations.until(EC.presence_of_element_located((By.XPATH, "//button[contains(@id,'showMoreButton')]")))
         more_destinations.click()
-        sleep(2)
     except:
         break
 
-wait_for_city_names = WebDriverWait(driver, 10)
-for button in wait_for_city_names.until(EC.visibility_of_all_elements_located((By.XPATH, "//div[contains(@class,'Explore-GridViewItem')]//button"))):
-    button.click()
-    print('clicked button for destination')
+sleep(2)
+destinations = driver.find_elements(By.XPATH, "//div[contains(@id,'destinations')]//button")
+
+flight_list = []
+
+for button in destinations[:2]:
+    flight_data = {}
+    action.click(button).perform()
 
     wait_for_div = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//div[contains(@class,'Explore-DestinationDetailsDrawerSection')]")))
 
     wait_for_info_btn = WebDriverWait(driver, 10)
     info_btn = wait_for_info_btn.until(EC.visibility_of_element_located(
         (By.XPATH, "//a[contains(@class, 'explore-clickout-button')]")))
+    flight_data['cities'] = driver.find_element(By.CLASS_NAME, 'clickout-box-title').text
+    flight_data['dates'] = driver.find_element(By.CLASS_NAME, 'clickout-box-subtitle').text
     info_btn.click()
-    current_window_handle = driver.current_window_handle
+    flight_data['link'] = info_btn.get_attribute('href')
 
+    current_window_handle = driver.current_window_handle
 
     # switch window to the new
     for window_handle in driver.window_handles:
         if window_handle != current_window_handle:
             driver.switch_to.window(window_handle)
 
-    wait_for_city_names = WebDriverWait(driver, 30)
-    wait_for_city_names.until(
+    wait_for_loaded_page = WebDriverWait(driver, 120)
+    wait_for_loaded_page.until(
         EC.visibility_of_element_located(
             (By.XPATH,
              "//div[contains(@id, 'advice') and (contains(text(), '¯\_(ツ)_/¯') or contains(text(), 'Buy now'))]")
         )
     )
 
-    wrapper_elements = wait_for_city_names.until(EC.visibility_of_all_elements_located(
+    wrapper_elements = wait_for_loaded_page.until(EC.visibility_of_all_elements_located(
         (By.XPATH, "//div[contains(@class, 'resultsContainer')]//div[contains(@class, 'wrapper')]")))
 
     cheapest_element = None
     for wrapper in wrapper_elements:
         if 'Cheapest' in wrapper.text:
             cheapest_element = wrapper.find_element(By.XPATH, ".//*[text()='Cheapest']")
-            print('*'*10)
-            print(wrapper.text)
-            print('*'*10)
+            flight_data['price'] = wrapper.find_element(By.XPATH, ".//*[contains(@class, 'price-section')]//a").text
             break
 
     if cheapest_element is not None:
-        cheapest_element = wait_for_city_names.until(EC.visibility_of(cheapest_element))
+        cheapest_element = wait_for_loaded_page.until(EC.visibility_of(cheapest_element))
 
-    if cheapest_element is None:
-        print("Cheapest element not found")
+    flight_list.append(flight_data)
 
     driver.close()
 
@@ -77,7 +85,10 @@ for button in wait_for_city_names.until(EC.visibility_of_all_elements_located((B
         (By.XPATH, "//button[contains(@data-type-id, 'Anywhere')]")))
     close_button.click()
 
-    print('closed destination')
-
+flight_dict = {"flights": flight_list}
+with open('flight_data.json', 'w') as f:
+    json.dump(flight_dict, f, indent=2)
 
 driver.quit()
+
+
